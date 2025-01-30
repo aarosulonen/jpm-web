403080334c00d3e2a07efc481a1959493f852c76
Aaro Sulonen <fatpipe.master@gmail.com>
2025-01-30 18:48:52 +0200

    Add image of Villisika to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,13 @@ export default function Home() {
   return (
     <div>
       <p>jpm pikkujoulut</p>
+      <Image
+            aria-hidden
+            src="/villisikaboom.jpg"
+            alt="Villisika"
+            width={1000}
+            height={1000}
+          />
     </div>
   );
 }
